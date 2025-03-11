5013b7fcf50f432972be6901f91447d3484a88f6
= <=>
2025-03-11 11:41:27 +0100

    feat: add is_product_available

--- stock_manager.py
+++ stock_manager.py
@@ -11,5 +11,30 @@ products = [
 print("=" * 50)
 print("Liste des produits disponibles")
 
+# Afficher les produits
+
 for product in products:
     print(f"{product['name']} - {product['price']} $ - {product['quantity']} en stock")
+    
+
+# verifier si un produit est disponible
+def is_product_available(product_name):
+    for product in products:
+        if product['name'] == product_name:
+            return True
+    return False
+
+rechercher_produit = input("Entrez le nom du produit que vous recherchez : ")
+
+if is_product_available(rechercher_produit.lower()):
+    if rechercher_produit.lower() in [product['name'] for product in products]:
+        for product in products:
+            if product['name'] == rechercher_produit.lower():
+                print(f"== Le produit {rechercher_produit.upper()} est disponible ==")
+                print(f"Prix : {product['price']} $")
+                print(f"Quantité : {product['quantity']} en stock")
+                break
+            else:
+                print(f"Le produit {rechercher_produit} n'est pas disponible")
+else:
+    print(f"Le produit {rechercher_produit} n'est pas disponible")
